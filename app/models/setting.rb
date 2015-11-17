b2516f0d284dd4d8b44f763ec1b31a8415e9eea2
Winston <winston.yongwei@gmail.com>
2015-11-17 10:14:17 +0800

    Expand send_at options

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -6,7 +6,7 @@ class Setting < ActiveRecord::Base
   end
 
   enumerize :send_at,
-            in: %w(01:00 03:00 05:00 07:00 09:00 11:00 13:00 15:00 17:00 19:00 21:00 23:00)
+            in: %w(00:00 01:00 02:00 03:00 04:00 05:00 06:00 07:00 08:00 09:00 10:00 11:00 12:00 13:00 14:00 15:00 16:00 17:00 18:00 19:00 20:00 21:00 22:00 23:00)
 
   enumerize :schedule,
             in: %w(everyday monday tuesday wednesday thursday friday saturday sunday),
